569aa7cb745a5da4cc7e9652e6c85a7bcf9a70d9
cemerso5 <cemerso5@vols.utk.edu>
2022-03-08 09:24:20 -0500

    Add Date Range

--- server.R
+++ server.R
@@ -1,7 +1,14 @@
 
 server <- function(input,output){
 output$ts_plot <- renderPlot({
+  min_date <- input$selected_date_range[1]
+  max_date <- input$selected_date_range[2]
+  
   plot_df <- stocks[stocks$symbol == input$selected_stock,]
+  
+  plot_df <- plot_df[plot_df$date >= min_date,]
+  plot_df <- plot_df[plot_df$date <= max_date,]
+  
   autoplot(plot_df, .vars = close)
 })
 
--- ui.R
+++ ui.R
@@ -2,6 +2,13 @@ ui <- fluidPage(
 selectInput(inputId = 'selected_stock',
               label = 'select stock',
               choices = unique(stocks$symbol)),
+dateRangeInput(
+  inputId = 'selected_date_range',
+  label = "Select date range",
+  start = min(stocks$date),
+  end = max(stocks$date),
+),
+
 plotOutput('ts_plot')
 
 )
