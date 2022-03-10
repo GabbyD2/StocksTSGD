f1fbafb5e32d772fd94c8b3f84c2c1588f22ba90
cemerso5 <cemerso5@vols.utk.edu>
2022-03-10 08:44:56 -0500

    Add metric to plot

--- server.R
+++ server.R
@@ -4,12 +4,13 @@ output$ts_plot <- renderPlot({
   min_date <- input$selected_date_range[1]
   max_date <- input$selected_date_range[2]
   
-  plot_df <- stocks[stocks$symbol == input$selected_stock,]
+  plot_df <- stocks[stocks$symbol == input$selected_stock,  
+                    c('date',input$selected_metric)]
   
   plot_df <- plot_df[plot_df$date >= min_date,]
   plot_df <- plot_df[plot_df$date <= max_date,]
   
-  autoplot(plot_df, .vars = close)
-})
-
-}
+  
+  autoplot(plot_df)
+        
+})}
